ff24ed6409274ddab935af88866bc1ce0ce56b45
ssoufii <salim_soufi@icloud.com>
2024-08-06 23:29:14 -0600

    setup box and stack from material ui

--- inventory-management-app/app/page.js
+++ inventory-management-app/app/page.js
@@ -1,6 +1,10 @@
-import {Box} from '@mui/material' //importing a box from material UI website
+import {Box,Stack} from '@mui/material' //importing a box from material UI website
 
 //page.js is similiar to app.js or index.html, this holds the main screen on the web application 
+
+
+const item = ['tomato', 'potato', 'mustard', 'ketchup', 'juice', 'watermelon']
+
 
 export default function Home() {
   return (
@@ -12,8 +16,40 @@ export default function Home() {
 
       alignItems = {'center'}
 
-      
+    >
+    
+    <Stack width = '800px' height = '600px' spacing = {2} >
+      {item.map((i) => (
+        <Box //this is how we create a flex box
+        key = {i}
+        width = '100%' //sets the width and height to fill entire screen
+        height = '100px'
+        display = {'flex'}
+        justifyContent = {'center'} //sets word to middle
+        bgcolor = {"#f0f0f0"}
+        alignItems = {'center'}
 
-    >Ello M8</Box>
+        >{i}
+          
+        </Box>
+
+
+
+      ))}
+
+
+    </Stack>
+    
+    
+    
+    
+    </Box>
+
+    
+
+
+
+
+
   );
 }
